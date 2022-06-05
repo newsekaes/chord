33b01e44e3dd3edf844ba07f8c070a46e0fcf7b9
newsekaes <282695040@qq.com>
2022-06-05 23:13:31 +0800

    fix:node_v16 npm config get

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
-// npm 自定义参数
-function getNpmConfig (name) {
-  // 解析指令参数
-  // npm指令需要读取 process.env.npm_config_argv
-  const argv = JSON.parse(process.env.npm_config_argv)
-  const config = {}
-  // 获取自定义参数
-  let idx = 2
-  const cooked = argv.cooked
-  const length = argv.cooked.length
-  while ((idx += 2) <= length) {
-    config[cooked[idx - 2]] = cooked[idx - 1]
-  }
-  return config[name]
-}
-
-process.env.DEPLOY_ENV = getNpmConfig('--dp-env') || 'default'
+process.env.DEPLOY_ENV = process.env.npm_config_dp_env || 'default'
 
 module.exports = {
   pages: {
